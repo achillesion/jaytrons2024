4750d6d7d1a099c0ca9eb92ce61c9e19ffcc3413
Ahmad <achillesion@gmail.com>
2026-07-16 16:02:02 +0500

    alignment on web and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Page() {
               </div>
             </div>
           </BlurFade>
-          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 max-w-[800px] mx-auto mb-8">
             {DATA.projects.map((project, id) => (
               <BlurFade
                 key={project.title}
